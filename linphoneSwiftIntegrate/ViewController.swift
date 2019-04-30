a9a01a74a0398a4d98b1b22e05672776175b4a63
ioio007 <hjenny3@gmail.com>
2019-04-30 20:33:40 +0800

    Initial Commit

---
+++ linphoneSwiftIntegrate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  linphoneSwiftIntegrate
+//
+//  Created by Jenny Huang on 2019/4/30.
+//  Copyright © 2019 Jenny Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
