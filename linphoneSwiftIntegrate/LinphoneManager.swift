41d2ad6c05b899371efe5f8a4e973cb7c668349e
ioio007 <hjenny3@gmail.com>
2019-04-30 21:21:49 +0800

    init comment

---
+++ linphoneSwiftIntegrate/LinphoneManager.swift
@@ -0,0 +1,9 @@
+//
+//  LinphoneManager.swift
+//  linphoneSwiftIntegrate
+//
+//  Created by Jenny Huang on 2019/4/30.
+//  Copyright © 2019 Jenny Huang. All rights reserved.
+//
+
+import Foundation
---
+++ linphoneSwiftIntegrate/linphone-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  linphone-Bridging-Header.h
+//  linphoneSwiftIntegrate
+//
+//  Created by Jenny Huang on 2019/4/30.
+//  Copyright © 2019 Jenny Huang. All rights reserved.
+//
+
+#ifndef linphone_Bridging_Header_h
+#define linphone_Bridging_Header_h
+
+
+#endif /* linphone_Bridging_Header_h */
